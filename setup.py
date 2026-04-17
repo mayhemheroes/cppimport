41d11c8e6cde360362bd73148837dcd9dc0e4401
Kuba Streit <kuba.streit@gmail.com>
2026-04-17 16:01:01 +0200

    Upgrade tools and packages

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 description = open("README.md").read()
 
 setup(
-    use_scm_version={"version_scheme": "post-release"},
-    setup_requires=["setuptools_scm"],
     packages=["cppimport"],
     install_requires=["mako", "pybind11", "filelock", "setuptools"],
     zip_safe=False,
@@ -23,7 +21,6 @@ setup(
         "Operating System :: OS Independent",
         "Operating System :: POSIX",
         "Topic :: Software Development",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: C++",
     ],
